06f16af22d15206e2d73712fee1ecfb1219994d5
KangSDhi <kangteknisi@gmail.com>
2022-05-02 20:35:20 +0700

    first commit

---
+++ migrations/20220502132007_create_etalase_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('etalase', function (table) {
+        table.increments('id');
+        table.string('nama', 255).notNullable();
+        table.timestamps();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable('etalase');
+};
